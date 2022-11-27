cb5327c6361158734f1c9979676c25b2ccc4f490
1DOTNETWORK <114791639+1DOTNETWORK@users.noreply.github.com>
2022-11-27 14:30:40 -0600

    Add files via upload

--- games/games.js
+++ games/games.js
@@ -36,6 +36,9 @@ newGame('./gameFiles/Breaking The Bank.png', 'Breaking The Bank')
 newGame('./gameFiles/Duck Life 1.png', 'Duck Life')
 newGame('./gameFiles/Duck Life 2.png', 'Duck Life 2')
 newGame('./gameFiles/DUCK LIFE 3.png', 'Duck Life 3')
+newGame('./gameFiles/DUCK LIFE 4.jpg', 'Duck Life 4')
+newGame('./gameFiles/Duck Life 5.png', 'Duck Life 5')
+newGame('./gameFiles/DUCK LIFE TREASURE HUNT.jfif', 'Duck Life TREASURE HUNT')
 newGame('./gameFiles/doublewires.png', 'Double Wires')
 newGame('./gameFiles/Idle Breakout.png', 'Idle Breakout')
 newGame('./gameFiles/Crossy Road.png', 'Crossy Road')
@@ -66,12 +69,21 @@ newGame('./gameFiles/Happy Wheels.jpg', 'Happy Wheels')
 newGame('./gameFiles/Rocket League.png', 'Rocket League')
 newGame('./gameFiles/Drift Boss.jpg', 'Drift Boss')
 newGame('./gameFiles/Basket Random.png', 'Basket Random')
-
-
-
-
-
-
+newGame('./gameFiles/Flippy Fish.png', 'Flippy Fish')
+newGame('./gameFiles/Fnaf 1.jpeg', 'Fnaf 1')
+newGame('./gameFiles/Fnaf 2.jpeg', 'Fnaf 2')
+newGame('./gameFiles/Fnaf 3.jpg', 'Fnaf 3')
+newGame('./gameFiles/Fnaf 4.jpg', 'Fnaf 4')
+newGame('./gameFiles/Fnaf SL.png', 'Fnaf Sister Location')
+newGame('./gameFiles/Basket Random.png', 'Basket Random')
+newGame('./gameFiles/Among Us.png', 'Among Us')
+newGame('./gameFiles/1v1lol.png', '1v1.lol')
+newGame('./gameFiles/Tiny Fishing.jpg', 'Tiny Fishing')
+newGame('./gameFiles/Friday Night Funkin.png', 'Friday Night Funkin')
+newGame('./gameFiles/Rooftop Snipers.png', 'Rooftop Snipers')
+newGame('./gameFiles/Krunker.jpg', 'Krunker')
+newGame('./gameFiles/Friday Night Funkin.png', 'Friday Night Funkin')
+newGame('./gameFiles/Rooftop Snipers.png', 'Rooftop Snipers')
 
 
 
@@ -83,6 +95,9 @@ var rowsTop = {
     4: '94%',
     5: '122%',
     6: '150%'
+}
+for (let i = 0; i < 100; i++) {
+    rowsTop[i] = ((i * 29) - 20).toString() + '%'
 }
 for (let i = 0; i < gameArr.length; i++) {
     // y pos
@@ -101,8 +116,38 @@ for (let i = 0; i < gameArr.length; i++) {
     if (i > 35) {
         gameArr[i].style.top = rowsTop[5]
     }
-    if (i >= 45) {
+    if (i > 44) {
         gameArr[i].style.top = rowsTop[6]
+    }
+    if (i > 53) {
+        gameArr[i].style.top = rowsTop[7]
+    }
+    if (i > 62) {
+        gameArr[i].style.top = rowsTop[8]
+    }
+    if (i > 71) {
+        gameArr[i].style.top = rowsTop[9]
+    }
+    if (i > 80) {
+        gameArr[i].style.top = rowsTop[10]
+    }
+    if (i > 89) {
+        gameArr[i].style.top = rowsTop[11]
+    }
+    if (i > 98) {
+        gameArr[i].style.top = rowsTop[12]
+    }
+    if (i > 107) {
+        gameArr[i].style.top = rowsTop[13]
+    }
+    if (i > 116) {
+        gameArr[i].style.top = rowsTop[14]
+    }
+    if (i > 125) {
+        gameArr[i].style.top = rowsTop[15]
+    }
+    if (i > 134) {
+        gameArr[i].style.top = rowsTop[16]
     }
     // x pos
     if (i <= 8) {
@@ -122,5 +167,20 @@ for (let i = 0; i < gameArr.length; i++) {
     }
     if (i >= 45) {
         gameArr[i].style.left = (i * 10 + 5 - 450).toString() + '%'
+    }
+    if (i >= 54) {
+        gameArr[i].style.left = (i * 10 + 5 - 540).toString() + '%'
+    }
+    if (i >= 63) {
+        gameArr[i].style.left = (i * 10 + 5 - 630).toString() + '%'
+    }
+    if (i >= 72) {
+        gameArr[i].style.left = (i * 10 + 5 - 720).toString() + '%'
+    }
+    if (i >= 81) {
+        gameArr[i].style.left = (i * 10 + 5 - 810).toString() + '%'
+    }
+    if(i >= 90) {
+        gameArr[i].style.left = (i * 10 + 5 - 900).toString() + '%'
     }
 }
