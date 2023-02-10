bc960d459111d6c06be5c8d6b477fd3187cf7382
Tomato <116928548+TomatoPng@users.noreply.github.com>
2023-02-10 16:38:32 -0600

    asdasd

--- settings/settings.js
+++ settings/settings.js
@@ -1,5 +1,10 @@
+const settingsContainer = document.getElementById('settingsContainer')
+
 function newSetting(name) {
     var setting = document.createElement('div')
     setting.setAttribute('class', 'setting')
     setting.innerText = name
+    settingsContainer.append(setting)
 }
+
+newSetting('test')
