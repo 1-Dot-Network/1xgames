a8b77d0adaa81be87111c411a1615f0917610adf
Tomato <116928548+TomatoPng@users.noreply.github.com>
2023-02-10 20:45:43 -0600

    asdasdad

--- settings/settings.js
+++ settings/settings.js
@@ -3,8 +3,10 @@ const settingsContainer = document.getElementById('row1')
 function newSetting(name, options) {
     var setting = document.createElement('div')
     var optionId = 0
-    localStorage.setItem(name, options[optionId])
-    localStorage.setItem(name + 'OID', optionId)
+    if (localStorage.getItem(name) === null) {
+        localStorage.setItem(name, options[optionId])
+        localStorage.setItem(name + 'OID', optionId)
+    }
     var optionLength = options.length
     setting.setAttribute('class', 'setting')
     setting.innerText = name + ' : ' + options[0]
