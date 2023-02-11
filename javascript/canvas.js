931168d3ce304562112205a95993df49bf39729c
Tomato <116928548+TomatoPng@users.noreply.github.com>
2023-02-10 19:50:59 -0600

    asdasd

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -409,4 +409,6 @@ function setup() {
     starfield.startRenderLoop();
 
 }
-window.onload = setup();
+if (localStorage.getItem('background') !== 'off') window.onload = setup(); else {
+    return
+}
